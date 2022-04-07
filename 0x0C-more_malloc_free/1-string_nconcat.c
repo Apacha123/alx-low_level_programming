a7edc450927936bb5eae34a257f36ddf8bc794b4
Apacha123 <apacha336@gmail.com>
2022-04-08 02:02:07 +0300

    --

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,19 @@
 #include "main.h"
+#include "stdlib.h"
 
 /**
-* string_nconcat - concatenates two strings.
-* @s1: first string
-* @s2: second string
-* @n: index
-* Return: char pointer
-*/
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ * malloc_checked - calls exit on failure
+ * @b: number of bytes to allocate
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
 {
-char *p;
-unsigned int size1 = 0, size2 = 0, i;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[size1] != '\0')
-{
-size1++;
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
-while (s2[size2] != '\0')
-{
-size2++;
-}
-if (n > size2)
-n = size2;
-p = malloc((size1 + n + 1) * sizeof(char));
-if (p == NULL)
-return (0);
-for (i = 0; i < size1; i++)
-{
-p[i] = s1[i];
-}
-for (; i < (size1 + n); i++)
-{
-p[i] = s2[i - size1];
-}
-p[i] = '\0';
-return (p);
-}
+
+
