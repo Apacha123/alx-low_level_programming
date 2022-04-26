22ef66f24043947b9dbf2360d8d1d41fbe92aa79
Apacha123 <apacha336@gmail.com>
2022-04-27 00:50:03 +0300

    Update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,34 +1,26 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * index of a listint_t linked list
- * @head: double pointer to head of list
- * @index: index of node to be deleted
- * Return: 1 if success, 1 if fail
+ * reverse_listint - reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *current;
-	listint_t *next;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
 
-	register uint i;
-
-	if (!head || !(*head))
-		return (FAILURE);
-	current = *head;
-	if (!index)
+	while (*head)
 	{
-		*head = current->next;
-		free(current);
-		return (SUCCESS);
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
-	for (i = 0; current && i < index - 1; i++)
-		current = current->next;
-	if (!current || !(current->next))
-		return (FAILURE);
-	next = current->next->next;
-	free(current->next);
-	current->next = next;
-	return (SUCCESS);
+
+	*head = prev;
+
+	return (*head);
 }
+
